f1bf33b22654f690bdf01036f27cf844555ffe85
Mohammed Sameh <mohamed.sameh.abdelzaher@gmail.com>
2025-05-02 09:53:22 +0300

    removing

--- chatapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class ChatMessage(models.Model):
-    session_id = models.CharField(max_length=255)
-    role = models.CharField(max_length=10)
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
